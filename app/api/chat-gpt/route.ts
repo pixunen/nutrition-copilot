33c6fb7450fa6a394a880cac4fdef4227c5c02f8
Otto Piskonen <otto.piskonen@gmail.com>
2023-03-19 18:13:23 +0200

    chat gpt api to page completed

--- app/api/chat-gpt/route.ts
+++ app/api/chat-gpt/route.ts
@@ -1,25 +1,17 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
+export async function GET() {
   const { Configuration, OpenAIApi } = require("openai");
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
-  var respond;
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Hello world",
-    });
-    respond = completion.data.choices[0].text
-  } catch (error: any) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
-    }
-  }
-  return NextResponse.json({respond})
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: 'user', content: 'Hello world!' }
+    ]
+  });
+  const respond = completion.data.choices[0];
+  return NextResponse.json({respond});
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
-async function handleClick() {
-  const response = await fetch('http://localhost:3000/api/chat-gpt');
-  return response;
+interface chatGPTRespond {
+  respond: {
+    message: {
+      role: string,
+      content: string
+    },
+    finish_reason: string,
+    index: number
+  },
 }
+
+async function getData() {
+  //https://jsonplaceholder.typicode.com/todos/1
+  const res = await fetch('http://localhost:3000/api/chat-gpt/');
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+  return await res.json();
+}
+
 export default async function Home() {
-  const data = await handleClick();
-  
+  const data: chatGPTRespond = await getData();
 
   return (
     <>
       <h1>This is chatGPT demo</h1><div>
-        <p></p>
+        <p>{data.respond.message.content}</p>
       </div>
     </>
   )
