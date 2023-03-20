01a813f6a63fd77505ae96aad378e01ae7568b52
Otto Piskonen <otto.piskonen@gmail.com>
2023-03-20 19:15:15 +0200

    form and api with parametrrs

--- app/api/chat-gpt/route.ts
+++ app/api/chat-gpt/route.ts
@@ -1,17 +1,25 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request: Request) {
+  // Get the query params from url
+  const { searchParams } = new URL(request.url);
+
+  // Init the openAI client
   const { Configuration, OpenAIApi } = require("openai");
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
+
+  // Generate the chat completion
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
-      { role: 'user', content: 'Hello world!' }
+      { role: 'user', content: searchParams.get('question') }
     ]
   });
+
+  // Parse the answer and send it as JSON
   const respond = completion.data.choices[0];
   return NextResponse.json({respond});
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,23 +9,27 @@ interface chatGPTRespond {
   },
 }
 
-async function getData() {
-  //https://jsonplaceholder.typicode.com/todos/1
-  const res = await fetch('http://localhost:3000/api/chat-gpt/');
-  // Recommendation: handle errors
+async function getData(question: string) {
+  const res = await fetch(`http://localhost:3000/api/chat-gpt/?question=${question}`);
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data');
   }
+
   return await res.json();
 }
 
 export default async function Home() {
-  const data: chatGPTRespond = await getData();
+  const question = "Hello";
+  const data: chatGPTRespond = await getData(question);
 
   return (
     <>
       <h1>This is chatGPT demo</h1><div>
+        <form action="http://localhost:3000/api/chat-gpt/" method="GET">
+          <label htmlFor="name">Question:</label>
+          <input type="text" id="name" name="question" required />
+          <button type="submit">Submit</button>
+        </form>
         <p>{data.respond.message.content}</p>
       </div>
     </>
